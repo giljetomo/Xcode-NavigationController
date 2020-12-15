7df5eba3d64417ca4e7694a31f714fcca296b77f
Macbook Pro <macbookpro@Gil.local>
2020-12-14 22:13:57 -0800

    first commit

---
+++ House-NavigationController/DiningRoomViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DiningRoomViewController.swift
+//  House-NavigationController
+//
+//  Created by Macbook Pro on 2020-12-14.
+//
+
+import UIKit
+
+class DiningRoomViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func returnToMainDoor(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
